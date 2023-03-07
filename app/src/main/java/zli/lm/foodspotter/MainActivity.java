1b7198a77a85e2e94646055e26452999ceb4a1ed
Loris Müller <lomue@gmx.ch>
2023-03-07 09:43:23 +0100

    added content to MainActivity and created StartActivity

--- app/src/main/java/zli/lm/foodspotter/MainActivity.java
+++ app/src/main/java/zli/lm/foodspotter/MainActivity.java
@@ -2,13 +2,28 @@ package zli.lm.foodspotter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button start_voting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start_voting = findViewById(R.id.voting);
+        Intent intent = new Intent(this, StartActivity.class);
+
+        start_voting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
     }
 }
